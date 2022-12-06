e34f43652744ee2c10ca0d9d241ab24bcd62198f
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-12-06 14:24:20 -0300

    endpoint of fetch changed

--- arthur_piloto_e_vinicius_santos/admin/assets/js/typesFetch.js
+++ arthur_piloto_e_vinicius_santos/admin/assets/js/typesFetch.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const getTypes = async () => {
-    const url = `http://localhost:3030/v1/types`
+    const url = `http://localhost:3030/v1/product-types`
     const response = await fetch(url)
     const typesData = await response.json()
     return typesData
