da15bc9e1ff88803ca207b9f2f7261332ad72383
vini00784 <vo70346@gmail.com>
2022-12-14 10:43:46 -0300

    preparing environment for integration with the API

---
+++ arthur_piloto_e_vinicius_santos/client/assets/js/fetch's/typesFetch.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const getTypes = async () => {
+    const url = `http://localhost:3030/v1/product-types`
+    const response = await fetch(url)
+    const typesData = await response.json()
+    return typesData
+}
+
+export {
+    getTypes
+}
